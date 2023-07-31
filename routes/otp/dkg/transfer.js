b584bfc0b3e0f45840e608106cf13803785517d8
CosmiCloud <tybranha@gmail.com>
2023-07-31 19:47:16 -0400

    api bug fixes

--- routes/otp/dkg/transfer.js
+++ routes/otp/dkg/transfer.js
@@ -1,36 +1,36 @@
-require('dotenv').config()
-const express = require('express')
-const router = express.Router()
-const purl = require('url')
-const ethers = require('ethers')
-const queryTypes = require('../../../public/util/queryTypes')
-const mysql = require('mysql')
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+const purl = require("url");
+const ethers = require("ethers");
+const queryTypes = require("../../../public/util/queryTypes");
+const mysql = require("mysql");
 const othubdb_connection = mysql.createConnection({
   host: process.env.DBHOST,
   user: process.env.DBUSER,
   password: process.env.DBPASSWORD,
-  database: process.env.OTHUB_DB
-})
+  database: process.env.OTHUB_DB,
+});
 
-function executeOTHUBQuery (query, params) {
+function executeOTHUBQuery(query, params) {
   return new Promise((resolve, reject) => {
     othubdb_connection.query(query, params, (error, results) => {
       if (error) {
-        reject(error)
+        reject(error);
       } else {
-        resolve(results)
+        resolve(results);
       }
-    })
-  })
+    });
+  });
 }
 
-async function getOTHUBData (query, params) {
+async function getOTHUBData(query, params) {
   try {
-    const results = await executeOTHUBQuery(query, params)
-    return results
+    const results = await executeOTHUBQuery(query, params);
+    return results;
   } catch (error) {
-    console.error('Error executing query:', error)
-    throw error
+    console.error("Error executing query:", error);
+    throw error;
   }
 }
 
@@ -55,167 +55,258 @@ const mainnet_node_options = {
 const testnet_dkg = new DKGClient(testnet_node_options)
 const mainnet_dkg = new DKGClient(mainnet_node_options)
 
-router.get('/', async function (req, res) {
-  try{
-    url_params = purl.parse(req.url, true).query
-    ip = req.socket.remoteAddress
+router.get("/", async function (req, res) {
+  try {
+    url_params = purl.parse(req.url, true).query;
+    ip = req.socket.remoteAddress;
     if (process.env.SSL_KEY_PATH) {
-      ip = req.headers['x-forwarded-for']
+      ip = req.headers["x-forwarded-for"];
     }
 
-    type = `Transfer`
+    type = `Transfer`;
 
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
     if (!url_params.api_key) {
-      console.log(`Get request without authorization.`)
+      console.log(`Get request without authorization.`);
       resp_object = {
-        result: 'Authorization key not provided.'
-      }
-      res.send(resp_object)
-      return
+        result: "Authorization key not provided.",
+      };
+      res.send(resp_object);
+      return;
     }
 
-    apiSpamProtection = await queryTypes.apiSpamProtection()
+    apiSpamProtection = await queryTypes.apiSpamProtection();
 
-    api_key = url_params.api_key
+    api_key = url_params.api_key;
     permission = await apiSpamProtection
       .getData(type, url_params.api_key)
       .then(async ({ permission }) => {
-        return permission
+        return permission;
       })
-      .catch(error => console.log(`Error : ${error}`))
+      .catch((error) => console.log(`Error : ${error}`));
 
     if (permission == `no_user`) {
-      console.log(`No user found for api key ${url_params.api_key}`)
+      console.log(`No user found for api key ${url_params.api_key}`);
       resp_object = {
-        result: 'Unauthorized key provided.'
-      }
-      res.send(resp_object)
-      return
+        result: "Unauthorized key provided.",
+      };
+      res.send(resp_object);
+      return;
     }
 
     if (permission == `block`) {
-      console.log(`Request frequency limit hit from ${url_params.api_key}`)
+      console.log(`Request frequency limit hit from ${url_params.api_key}`);
       resp_object = {
         result:
-          'Request blocked by spam protection. Only 1 request is allow per 10 seconds.'
-      }
-      res.send(resp_object)
-      return
+          "Request blocked by spam protection. Only 1 request is allow per 10 seconds.",
+      };
+      res.send(resp_object);
+      return;
     }
 
     if (!url_params.ual) {
-        console.log(`Get request with no ual from ${url_params.api_key}`)
-        resp_object = {
-          result: 'No UAL provided.'
-        }
-        res.send(resp_object)
-        return
-      }
-  
-      const segments = url_params.ual.split(':');
-      const argsString = segments.length === 3 ? segments[2] : segments[2] + segments[3];
-      const args = argsString.split('/');
+      console.log(`Get request with no ual from ${url_params.api_key}`);
+      resp_object = {
+        result: "No UAL provided.",
+      };
+      res.send(resp_object);
+      return;
+    }
 
-      if (args.length !== 3) {
-        console.log(`Get request with invalid ual from ${url_params.api_key}`)
-        resp_object = {
-          result: 'Invalid UAL provided.'
-        }
-        res.send(resp_object)
-        return
-      }
+    const segments = url_params.ual.split(":");
+    const argsString =
+      segments.length === 3 ? segments[2] : segments[2] + segments[3];
+    const args = argsString.split("/");
 
-    if (!url_params.network || (url_params.network !== 'otp::testnet' && url_params.network !== 'otp::mainnet')) {
-      console.log(`Get request with invalid network from ${url_params.api_key}`)
+    if (args.length !== 3) {
+      console.log(`Get request with invalid ual from ${url_params.api_key}`);
+      resp_object = {
+        result: "Invalid UAL provided.",
+      };
+      res.send(resp_object);
+      return;
+    }
+
+    if (
+      !url_params.network ||
+      (url_params.network !== "otp::testnet" &&
+        url_params.network !== "otp::mainnet")
+    ) {
+      console.log(
+        `Get request with invalid network from ${url_params.api_key}`
+      );
       resp_object = {
         result:
-          'Invalid network provided. Current supported networks are: otp::testnet, otp::mainnet.'
-      }
-      res.send(resp_object)
-      return
+          "Invalid network provided. Current supported networks are: otp::testnet, otp::mainnet.",
+      };
+      res.send(resp_object);
+      return;
     }
 
-    if (!url_params.public_address || !ethers.utils.isAddress(url_params.public_address)) {
-        console.log(`Publish request with invalid public_address from ${url_params.api_key}`)
-        resp_object = {
-          result:
-            'Invalid public_address (evm address) provided.'
-        }
-        res.send(resp_object)
-        return
-      }
+    if (
+      !url_params.public_address ||
+      !ethers.utils.isAddress(url_params.public_address)
+    ) {
+      console.log(
+        `Publish request with invalid public_address from ${url_params.api_key}`
+      );
+      resp_object = {
+        result: "Invalid public_address (evm address) provided.",
+      };
+      res.send(resp_object);
+      return;
+    }
 
     if (!url_params.receiver || !ethers.utils.isAddress(url_params.receiver)) {
-        console.log(`Transfer request with invalid receiver address from ${url_params.api_key}`)
-        resp_object = {
-          result:
-            'Invalid receiver (evm address) provided.'
-        }
-        res.send(resp_object)
-        return
-      }
-
-      receiver = {
-        receiver: url_params.receiver
-      }
-
-    txn_description = url_params.txn_description
-    if(!url_params.txn_description){
-      txn_description = 'No description available.'
+      console.log(
+        `Transfer request with invalid receiver address from ${url_params.api_key}`
+      );
+      resp_object = {
+        result: "Invalid receiver (evm address) provided.",
+      };
+      res.send(resp_object);
+      return;
     }
 
-    query = `select * from user_header where api_key = ?`
-      params = [url_params.api_key]
-      user = await getOTHUBData(query, params)
-        .then(results => {
-          //console.log('Query results:', results);
-          return results
-          // Use the results in your variable or perform further operations
+    if (url_params.network === "otp::testnet") {
+      dkg_get_result = await testnet_dkg.asset
+        .getOwner(url_params.ual, {
+          validate: true,
+          maxNumberOfRetries: 30,
+          frequency: 1,
+          blockchain: {
+            name: url_params.network,
+            publicKey: process.env.PUBLIC_KEY,
+            privateKey: process.env.PRIVATE_KEY,
+          },
         })
-        .catch(error => {
-          console.error('Error retrieving data:', error)
+        .then((result) => {
+          //console.log(JSON.stringify(result))
+          return result;
         })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
 
-        query =
-          `INSERT INTO txn_header (txn_id, progress, public_address, api_key, request, network, app_name, txn_description, txn_data, ual, keywords, state, txn_hash, txn_fee, trac_fee, epochs) VALUES (UUID(),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`
-        await othubdb_connection.query(
-          query,
-          ['PENDING',url_params.public_address, url_params.api_key, type, url_params.network, user[0].app_name, txn_description, JSON.stringify(receiver), url_params.ual, null, null, null, null, null, null],
-          function (error, results, fields) {
-            if (error) throw error
-          }
-        )
+    if (url_params.network === "otp::mainnet") {
+      dkg_get_result = await mainnet_dkg.asset
+        .getOwner(url_params.ual, {
+          validate: true,
+          maxNumberOfRetries: 30,
+          frequency: 1,
+          blockchain: {
+            name: url_params.network,
+            publicKey: process.env.PUBLIC_KEY,
+            privateKey: process.env.PRIVATE_KEY,
+          },
+        })
+        .then((result) => {
+          //console.log(JSON.stringify(result))
+          return result;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
 
-        query = `select * from txn_header where api_key = ? and request = ? order by created_at desc`
-        params = [url_params.api_key, type]
-        txn = await getOTHUBData(query, params)
-          .then(results => {
-            //console.log('Query results:', results);
-            return results
-            // Use the results in your variable or perform further operations
-          })
-          .catch(error => {
-            console.error('Error retrieving data:', error)
-          })
+    if (!dkg_get_result || dkg_get_result.errorType) {
+      console.log(
+        `getOwner request with invalid ual from ${url_params.api_key}`
+      );
+      resp_object = {
+        result: "Error occured while getting asset owner.",
+      };
+      res.send(resp_object);
+      return;
+    }
+
+    // if (dkg_get_result.address !== url_params.receiver) {
+    //   console.log(
+    //     `Transfer requested for an asset the public_address did not own from ${url_params.api_key}`
+    //   );
+    //   resp_object = {
+    //     result: "This public_address does not own this asset.",
+    //   };
+    //   res.send(resp_object);
+    //   return;
+    // }
+
+    receiver = {
+      receiver: url_params.receiver,
+    };
+
+    txn_description = url_params.txn_description;
+    if (!url_params.txn_description) {
+      txn_description = "No description available.";
+    }
+
+    query = `select * from user_header where api_key = ?`;
+    params = [url_params.api_key];
+    user = await getOTHUBData(query, params)
+      .then((results) => {
+        //console.log('Query results:', results);
+        return results;
+        // Use the results in your variable or perform further operations
+      })
+      .catch((error) => {
+        console.error("Error retrieving data:", error);
+      });
+
+    query = `INSERT INTO txn_header (txn_id, progress, public_address, api_key, request, network, app_name, txn_description, txn_data, ual, keywords, state, txn_hash, txn_fee, trac_fee, epochs) VALUES (UUID(),?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+    await othubdb_connection.query(
+      query,
+      [
+        "PENDING",
+        url_params.public_address,
+        url_params.api_key,
+        type,
+        url_params.network,
+        user[0].app_name,
+        txn_description,
+        JSON.stringify(receiver),
+        url_params.ual,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+      ],
+      function (error, results, fields) {
+        if (error) throw error;
+      }
+    );
+
+    query = `select * from txn_header where api_key = ? and request = ? order by created_at desc`;
+    params = [url_params.api_key, type];
+    txn = await getOTHUBData(query, params)
+      .then((results) => {
+        //console.log('Query results:', results);
+        return results;
+        // Use the results in your variable or perform further operations
+      })
+      .catch((error) => {
+        console.error("Error retrieving data:", error);
+      });
 
     resp_object = {
-      result: 'Transfer transaction queued successfully.',
+      result: "Transfer transaction queued successfully.",
       public_address: url_params.public_address,
-      url: `${process.env.WEB_HOST}/portal/gateway?txn_id=${txn[0].txn_id}`
-    }
+      url: `${process.env.WEB_HOST}/portal/gateway?txn_id=${txn[0].txn_id}`,
+    };
 
-    res.json(resp_object)
-  }catch(e){
-    console.log(e)
-        resp_object = {
-            result: 'Oops, something went wrong! Please try again later.'
-        }
+    res.json(resp_object);
+  } catch (e) {
+    console.log(e);
+    resp_object = {
+      result: "Oops, something went wrong! Please try again later.",
+    };
 
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        res.json(resp_object)
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(resp_object);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
